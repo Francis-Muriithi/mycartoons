95fc6f118184d08d53addd6317e09930fcb86247
Francis Muriithi <mufraggie@gmail.com>
2022-10-07 02:58:03 +0300

    About Component

---
+++ src/Components/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function About() {
+  return (
+    <>
+<div class="about-section">
+  <h1> About Me</h1>
+  <h3>Francis Muriithi</h3>
+  <img src="https://img.favpng.com/12/22/25/web-development-programmer-software-development-software-developer-computer-software-png-favpng-Z3dPn4z7P67KZehEDkJph8Hqi.jpg" alt="" width="400" height="300"></img>
+  <p>I am a junior Software Developer. My committment to learning pushed me to enroll into a software engineering course at Moringa and I appreciate the skills and the development that the institution offers.</p>
+  <p>
+<strong>What brings me to where I am today…</strong>
+<br></br>
+At Moringa school, we are trained to develop innovative and creative products and services that provide total communication and information solutions. Among a plethora of services, web design and development, tailor made applications, ERPs, CRMs, e-commerce solutions, business-to-business applications, business-to-client applications, managed hosting and internet portal management are few that we offer. Satisfied clients around the globe will bear testimony to the quality of our work.
+<br></br>
+Building on this reputation and my experience so far, I hope to grow and mature into the profession for career and personal development.</p>
+</div>
+  </>
+  )
+}
+
+export default About;
